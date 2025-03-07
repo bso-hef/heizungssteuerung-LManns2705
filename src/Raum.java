c5768831028151266f724db5c76850dd67caa712
lmanns0507 <linus-manns@web.de>
2025-03-07 09:11:49 +0100

    Jan, Linus, Justin

--- src/Raum.java
+++ src/Raum.java
@@ -1,28 +1,65 @@
-public class Raum {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Raum {
     private int raumId;
+
     private int zieltemperatur;
 
-    public Raum (int raumId, int zieltemperatur) {
+    private List<Fenster> fensterliste = new ArrayList<>();
+
+    private List<Heizung> heizungliste = new ArrayList<>();
+
+    private Thermometer thermometer;
+
+    public Raum(int raumId, int zieltemperatur) {
         this.raumId = raumId;
         this.zieltemperatur = zieltemperatur;
+    }
+
+    public boolean hatOffeneFenster() {
+        for (Fenster fenster:fensterliste) {
+            if (fenster.isOffen()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void regeleTemperatur() {
+       int aktuelleTemperatur = this.thermometer.getTemperatur();
+           if(hatOffeneFenster()) {
+               for (Heizung heizung : heizungliste) {
+                   heizung.schalteAus();
+               }
+           } else {
+               if (aktuelleTemperatur >= zieltemperatur) {
+                   for (Heizung heizung : heizungliste) {
+                       heizung.schalteAus();
+                   }
+               } else {
+                   for (Heizung heizung : heizungliste) {
+                       heizung.schalteEin();
+                   }
+               }
+           }
+       }
+
+
+    public List<Heizung> getHeizungliste() {
+        return heizungliste;
     }
 
     public int getZieltemperatur() {
         return zieltemperatur;
     }
 
-    public void setZieltemperatur() {
+    public void setZieltemperatur(int zieltemperatur) {
         this.zieltemperatur = zieltemperatur;
     }
 
-    public boolean hatOffeneFenster() {
-    }
-
-    public void regeleTemperatur() {
-    }
-
+    @Override
     public String toString() {
-        return "Raum" + raumId + ": " + zieltemperatur + "°C. Fenster: ";
+        return "RaumId: " + raumId + "Zieltemperatur: " + zieltemperatur + "°C" + "Fenster" + fensterliste.size() + "Heizungen: " + heizungliste.size();
     }
 }
